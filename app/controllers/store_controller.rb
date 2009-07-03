292f18d20e19d07da8e685e19b0aeafe2e1ef8bb
magno <magno.gutierrez@gmail.com>
2009-07-03 13:51:27 -0500

    subiendo avance again

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,10 @@
 class StoreController < ApplicationController
+  
   def index
     @products = Product.find_products_for_sale
-		@cart = find_cart
+    @cart = find_cart
   end
+  
 
 
   
@@ -10,10 +12,11 @@ class StoreController < ApplicationController
   def add_to_cart
     product = Product.find(params[:id])
     @cart = find_cart
-    @cart.add_product(product)
-		respond_to do |format|
-			format.js
-		end
+    @current_item = @cart.add_product(product)
+      respond_to do |format|
+        format.js if request.xhr?
+        format.html {redirect_to_index}
+      end
   rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid product #{params[:id]}")
     redirect_to_index("Invalid product")
@@ -24,7 +27,7 @@ class StoreController < ApplicationController
   
   def empty_cart
     session[:cart] = nil
-    redirect_to_index("Your cart is currently empty")
+    redirect_to_index
   end
   
 
